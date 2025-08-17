4952c628ac8c791925e1b5eae336c38e16549792
Harshith9Aileneni <aileneni.h@northeastern.edu>
2025-08-16 22:06:28 -0400

    File manger class Complete

--- contact_manager_class.py
+++ contact_manager_class.py
@@ -4,11 +4,14 @@ class ContactManager:
     def __init__(self):
         print("creating Contact Manager@")
         self.contacts = []
+        self.load_from_file()
 
     def add_contacts(self,name,phone,email):
         new_contact = {"name" : name, "phone" : phone, "email" : email}
         self.contacts.append(new_contact)
+        self.save_to_file()
         print(f"Contact with name: {name} has been successfully created")
+
 
 
     def show_all_contacts(self):
@@ -21,6 +24,22 @@ class ContactManager:
 
     def count_contacts(self):
         return len(self.contacts)
+
+    def save_to_file(self):
+        with open("contacts.json","w") as file:
+            json.dump(self.contacts, file, indent=2)
+
+    def load_from_file(self):
+        try:
+            with open("contacts.json", "r") as file:
+                self.contacts = json.load(file)
+                print(f"loaded {len(self.contacts)} Contacts")
+        except FileNotFoundError:
+            print("Yoo bro, I can't find that Contact")
+
+
+
+
 
 
 manager = ContactManager()
